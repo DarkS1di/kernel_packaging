53378cd76e27f453b57ce224fac32951fb9c7fec
NoxS1d <148648283+NoxS1d-Dev@users.noreply.github.com>
2026-03-16 02:52:24 +0200

    Update kernel string in anykernel.sh

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Electro Kernel by DarkS1di
+kernel.string=Electro Kernel by NoxS1d
 do.devicecheck=1
 do.modules=0
 do.systemless=1
