5e69665ec80948ebd56f27fe9e95fdc30b0e2c49
NoxS1d <148648283+NoxS1d-Dev@users.noreply.github.com>
2026-04-29 13:03:11 +0300

    packaging: update for ElectroX rebranding

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Electro Kernel by NoxS1d
+kernel.string=ElectroX Kernel by NoxS1d
 do.devicecheck=1
 do.modules=0
 do.systemless=1
